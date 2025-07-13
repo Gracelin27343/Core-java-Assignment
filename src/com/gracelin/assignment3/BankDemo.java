653590766e664e4715f5c3c91713e9a25ee88d3f
Gracelin <jgracelin15@gmail.com>
2025-07-13 15:30:02 +0530

    Banking Transaction System that incorporates static, abstract, and final

---
+++ src/com/gracelin/assignment3/BankDemo.java
@@ -0,0 +1,24 @@
+package com.gracelin.assignment3;
+
+public class BankDemo {
+
+	public static void main(String[] args) {
+        Bank bank1 = new Bank();
+        Bank bank2 = new Bank();
+
+        System.out.println("Total Accounts: " + Bank.getTotalAccounts());
+
+        Account savings = new SavingsAccount("Grace", 5000);
+        Account checking = new CheckingAccount("Geo", 3000);
+
+        Account savings1 = new SavingsAccount("harini", 5000);
+        Account checking1 = new CheckingAccount("jeni", 3000);
+
+        Transaction txn = new Transaction();
+        txn.performTransaction(savings, "deposit", 1000);
+        txn.performTransaction(checking, "withdraw", 500);
+        
+        txn.performTransaction(savings1, "deposit", 10000);
+        txn.performTransaction(checking1, "withdraw", 900);
+    }
+}
